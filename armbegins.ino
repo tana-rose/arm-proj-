c4b15eaa8829e62069bf963cb3462bc3400145ac
tana-rose <152645288+tana-rose@users.noreply.github.com>
2024-09-30 12:36:50 -0400

    Update armbegins.ino
    added one maybe two more motors

--- armbegins.ino
+++ armbegins.ino
@@ -4,6 +4,8 @@ Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
+Servo servo5;
+Servo servo6;
 
 int pos1 = 0;  //first motors position
 int poten1;    // first potentiometer
@@ -16,26 +18,40 @@ int poten3;
 
 int pos4 = 0;
 int poten4;
+
+int pos5 = 0;
+int poten5;
+
+int pos6 = 0;
+int poten6;
 void setup() {
   Serial.begin(9600);
-  servo1.attach(9);
+  
+  servo1.attach(10);
 
-  servo2.attach(8);
+  servo2.attach(9);
 
-  servo3.attach(7);
+  servo3.attach(8);
 
-  servo4.attach(6);
+  servo4.attach(7);
+
+  servo5.attach(6);
+
+  servo6.attach(5);
 }
 
 void loop() {
+  poten1 = analogRead(0);
 
-  poten1 = analogRead(1);
+  poten2 = analogRead(1);
 
-  poten2 = analogRead(2);
+  poten3 = analogRead(2);
 
-  poten3 = analogRead(3);
+  poten4 = analogRead(3);
 
-  poten4 = analogRead(4);
+  poten5 = analogRead(4);
+
+  poten6 = analogRead(5);
 
   pos1 = map(poten1, 0, 1023, 0, 180);
 
@@ -44,6 +60,10 @@ void loop() {
   pos3 = map(poten3, 0, 1023, 0, 180);
 
   pos4 = map(poten4, 0, 1023, 0, 180);
+  
+  pos5 = map(poten5, 0, 1023, 0, 180);
+
+  pos6 = map(poten6, 0, 1023, 0, 180);
 
   Serial.print("{1}");
    Serial.print(poten1);
@@ -64,9 +84,22 @@ void loop() {
   Serial.print("\t");
 
   Serial.print("{4}");
-   Serial.print(poten4);
+    Serial.print(poten4);
+      Serial.print("/");
+        Serial.print(pos4);
+  Serial.print("\t");
+
+    Serial.print("{5}");
+   Serial.print(poten5);
      Serial.print("/");
-       Serial.print(pos4);
+       Serial.print(pos5);
+  Serial.print("\t");
+
+
+  Serial.print("{6}");
+   Serial.print(poten6);
+     Serial.print("/");
+       Serial.print(pos6);
   Serial.println();
 
 
@@ -74,6 +107,8 @@ void loop() {
   servo2.write(pos2);
   servo3.write(pos3);
   servo4.write(pos4);
-
+  servo5.write(pos5);
+  servo6.write(pos6);
   delay(50);
 }
+
